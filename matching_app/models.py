72e0b4ced331d5eff44eeac6f4448deac518337c
pcjs156 <itqkrdbqls1001@naver.com>
2023-04-27 01:19:58 +0900

    feat: Matching model에 host 필드 추가

--- matching_app/models.py
+++ matching_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Matching(models.Model):
+    host = models.ForeignKey('user_app.User', null=False, blank=False, on_delete=models.CASCADE,
+                             related_name='hosted_matchings', verbose_name='주최자')
     starts_at = models.DateTimeField(verbose_name='모임 시작 일시')
     join_deadline = models.DateTimeField(verbose_name='인원 모집 마각 일시')
     place = models.CharField(max_length=30, verbose_name='모임 장소')
