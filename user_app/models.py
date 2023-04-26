d3c80563b9c6ff66e12628811acc5a3612aa30d5
pcjs156 <itqkrdbqls1001@naver.com>
2023-04-26 23:54:37 +0900

    feat: Custom User model 추가

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    email = models.EmailField(null=True, blank=True)
+
+    name = models.CharField(max_length=100, verbose_name='이름')
