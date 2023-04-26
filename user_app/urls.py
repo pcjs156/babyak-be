cae48c6242cd1ab82fa8110268b02c87ee24645b
2018-15596 <syleelsw@snu.ac.kr>
2023-04-27 07:54:09 +0900

    add logout

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from user_app.views import LoginAPIView, RegistrationAPIView
+from user_app.views import LoginAPIView, RegistrationAPIView, LogoutAPIView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login', LoginAPIView.as_view(), name='login'),
-    path('signup', RegistrationAPIView.as_view(), name='signup'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('signup/', RegistrationAPIView.as_view(), name='signup'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
 ]
