33a5aed659dace06073fedba21c204ab5223abb8
pcjs156 <itqkrdbqls1001@naver.com>
2023-04-27 00:53:56 +0900

    feat: Matching model 생성

--- matching_app/models.py
+++ matching_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Matching(models.Model):
+    starts_at = models.DateTimeField(verbose_name='모임 시작 일시')
+    join_deadline = models.DateTimeField(verbose_name='인원 모집 마각 일시')
+    place = models.CharField(max_length=30, verbose_name='모임 장소')
+    category = models.CharField(max_length=30, verbose_name='모임 유형')
+    description = models.TextField(blank=True, default='', verbose_name='추가 정보')
+    people_limit = models.PositiveIntegerField(null=True, blank=True, verbose_name='최대 인원')
+    joined_members = models.ManyToManyField('user_app.User', related_name='joined_matchings', verbose_name='참여 유저')
