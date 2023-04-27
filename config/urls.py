d60f4017176b26ea031c82a14c2a4dfbefff5466
pcjs156 <itqkrdbqls1001@naver.com>
2023-04-28 00:25:33 +0900

    fix: 제발

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 
 from . import views
 
-urlpatterns = [
-    path('admin', admin.site.urls),
+apiurlpatterns = [
     path('', views.server_check, name='server-check'),
     path('users/', include('user_app.urls')),
     path('matchings/', include('matching_app.urls')),
+]
+
+urlpatterns = [
+    path('admin', admin.site.urls),
+    path('api/', include(apiurlpatterns)),
 ]
