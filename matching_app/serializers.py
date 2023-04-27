c1f95da1a9b1daa120be32dea187b8e991f44ad5
pcjs156 <itqkrdbqls1001@naver.com>
2023-04-28 03:27:36 +0900

    feat:

--- matching_app/serializers.py
+++ matching_app/serializers.py
@@ -7,8 +7,8 @@ from .models import Matching
 
 class MatchingSerializer(serializers.ModelSerializer):
     status = serializers.CharField(read_only=True)
-    host = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
-    joined_members = serializers.SlugRelatedField(many=True, read_only=True, slug_field='username')
+    host = serializers.SlugRelatedField(many=False, read_only=True, slug_field='name')
+    joined_members = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
 
     class Meta:
         model = Matching
